cc9f3476560c3d0e146d66cc0952b304564abf0b
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-07-02 13:54:30 -0400

    some crappy jquery timer code

--- clicky.js
+++ clicky.js
@@ -9,3 +9,36 @@ $(document).ready(function() {
 		});
 	})
 });
+
+//TIMER 
+function startTimer(duration, display, button) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        if(seconds == 00){
+			display.text("TIME UP!");
+        }
+        else{
+        	display.text(minutes + ":" + seconds);
+        }
+        	
+        button.text("RESET");
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+$( "#startAndStop" ).click(function() {
+	var time = 10, //in seconds
+        display = $('#time'),
+        button = $('#startAndStop');
+    startTimer(time, display, button);
+});
+
+});
