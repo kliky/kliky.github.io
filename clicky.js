bf017e0991adf1c68637f4c10cc6ca4865466ced
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-07-02 15:16:03 -0400

    update

--- clicky.js
+++ clicky.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 	var num = 0;
 	$('#clickThis').click(function() {
 		
-        if(num == 0){
-        var time = 60, //in seconds
-            display = $('#time'),
-            button = $('#startAndStop');
-        startTimer(time, display, button);
+        if(num < 1){
+            var time = 6, //in seconds
+                display = $('#time'),
+                button = $('#startAndStop');
+        
+            var status =  startTimer(time, display, button, num);
         }
         num += 1;
 		$('#number').html(function(i, val) {
@@ -19,7 +20,7 @@ $(document).ready(function() {
 });
 
 //TIMER 
-function startTimer(duration, display, button) {
+function startTimer(duration, display, button, num) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -31,6 +32,8 @@ function startTimer(duration, display, button) {
 			display.text("TIME UP!");
             button.text("PLAY AGAIN!");
             timer.stop();
+            num = 0;
+            return 1
         }
         else{
         	display.text("You have " + minutes + ":" + seconds + " seconds!");
